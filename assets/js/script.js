4790ed0b414eec03408beb5051b6045ffee07cfb
mich-hales <michellehales93@gmail.com>
2022-08-15 13:24:59 -0600

    showquestion function is not working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ let flexBox = document.querySelector('.flex-container');
 function startQuiz() {
     questionContainer.classList.remove('hide');
     startBtn.classList.add('hide');
+    showQuestion(quizQuestions);
 }
 
 // seconds left
@@ -41,4 +42,79 @@ function sendMessage() {
 
 startButton.addEventListener('click', startTimer);
 startButton.addEventListener('click', startQuiz);
+
+
+let quizQuestions = [{
+    question: 'Inside which element do you put JavaScript?',
+    answers: ['<var>', 
+    '<script>', 
+    '<section>', 
+    '<code>'],
+    correctAnswer: 1
+    }, {
+    question: 'Which of the following reverses the order of the elements of an array?',
+    answers: ['reverse()',
+    'push()',
+    'reduce()',
+    'reduceRight()'],
+    correctAnswer: 0
+    }, {
+    question: 'How do you get the DOM element with id in JavaScript?',
+    answers: ['window.getElementById(...)',
+    'document.innerHTML.getElementById(...)',
+    'page.getElementById(...)',
+    'document.getElementById(...)'],
+    correctAnswer: 3
+}, {
+    question: 'Which built-in method combines the text of two strings and returns a new string?',
+    answers: ['append()',
+    'concat()',
+    'attach()',
+    'None of the above'],
+    correctAnswer: 1
+}, {
+    question: 'Which of the following creates a new array with all of the elements of this array for which the provided filtering function returns true?',
+    answers: ['concat()',
+    'every()',
+    'filter()',
+    'some()'],
+    correctAnswer: 2
+}];
+
+
+// selecting dom elements
+let questionTitle = document.querySelector('#quiz-question');
+let answerOptions = document.querySelectorAll('.answers');  
+
+function showQuestion(q) {
+    questionTitle.textContent = q.question;
+
+    answerOptions.forEach(function(element, index){
+        element.textContent = q.answers[index];
+          
+        element.addEventListener('click', function(){
+
+            if (q.correctAnswer == index) {
+                console.log('you are right!');
+            } else {
+                console.log('you are wrong!');
+            }
+        });
+    }); 
+}
+
+
+
+
+
+
+// function to get questions to cycle through
+
+// record scores
+
+// subtract time if answer is wrong
+
+// save scores and rank them with their initials
+
+// restart quiz
 
